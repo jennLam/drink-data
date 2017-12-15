f2bd6d150abda63a54ab938774b0acb09da2f0f5
vagrant <jennaylam@gmail.com>
2017-12-15 22:57:39 +0000

    Added retailer affinity table.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from jinja2 import StrictUndefined
 import json
 import data
@@ -12,7 +12,8 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return render_template("home.html", brands=get_brands())
+    return render_template("home.html", brands=get_brands(),
+                           top_buy_rate_brand=data.top_buying_brand())
 
 
 def get_brands():
@@ -34,8 +35,19 @@ def get_rates():
     return lst_of_rates
 
 
+@app.route("/affinity.json")
+def get_affinity():
+    brand = request.args.get("b")
 
-@app.route('/buy-rate.json')
+    print brand
+
+    affinity = data.get_retailer_affinity_values(brand)
+    top_affinity = data.retailer_affinity(brand)
+
+    return jsonify({"brand": brand, "affinity": affinity, "top_affinity": top_affinity})
+
+
+@app.route("/buy-rate.json")
 def buying_rates():
     """Return data about number of events."""
 
@@ -54,27 +66,17 @@ def buying_rates():
     for brand in lst_of_brands:
         lst_of_rates.append(brand_buy_rate[brand])
 
-    data_dict = {
-                "labels": lst_of_brands,
-                "datasets": [
-                    {
-                        "data": lst_of_rates,
-                        "label": "Buying Rate ($ Spent / HH)",
-                        "backgroundColor": [
-                            "#c70039",
-                            "#ff5733",
-                            "#ff8d1a",
-                            "#ffc300",
-                        ],
-                        "hoverBackgroundColor": [
-                            "#c70039",
-                            "#3ff5733",
-                            "#Fff8d1a",
-                            "#3ffc300",
-                        ]
-                    }]
-
-                }
+    data_dict = {"labels": lst_of_brands,
+                 "datasets": [{"data": lst_of_rates,
+                               "label": "Buying Rate ($ Spent / HH)",
+                               "backgroundColor": ["#c70039",
+                                                   "#ff5733",
+                                                   "#ff8d1a",
+                                                   "#ffc300"],
+                               "hoverBackgroundColor": ["#c70039",
+                                                        "#3ff5733",
+                                                        "#Fff8d1a",
+                                                        "#3ffc300"]}]}
 
     return jsonify(data_dict)
 
